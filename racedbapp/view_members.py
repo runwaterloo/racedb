97ac506df163913ad1bd82ed711adf01862a68fb
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2017-03-08 21:59:36 -0500

    Add camera column to members view

--- racedbapp/view_members.py
+++ racedbapp/view_members.py
@@ -6,7 +6,10 @@ from .models import *
 
 def index(request):
     members = Rwmember.objects.filter(active=True)
+    member_tags = list(Phototag.objects.values_list('tag', flat=True).distinct().filter(tag__startswith='m'))
+    member_tags = [int(x.lstrip('m')) for x in member_tags]
     context = {
                'members': members,
+               'member_tags': member_tags,
               }
     return render(request, 'racedbapp/members.html', context)
