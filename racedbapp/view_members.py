85dadb8b9d5cf5933cedbdfcd1a015fbde3d1a5e
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2017-03-21 18:03:15 -0400

    Start relying on hasphotos instead of phototags

--- racedbapp/view_members.py
+++ racedbapp/view_members.py
@@ -6,11 +6,7 @@ from .models import *
 
 def index(request):
     members = Rwmember.objects.filter(active=True)
-    member_tags = list(Phototag.objects.values_list('tag', flat=True).distinct().filter(tag__startswith='m'))
-    member_tags = [x.lstrip('m') for x in member_tags]
-    member_tags = [int(x) for x in member_tags if x.isdigit()]
     context = {
                'members': members,
-               'member_tags': member_tags,
               }
     return render(request, 'racedbapp/members.html', context)
