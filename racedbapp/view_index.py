08a007c1ad88ba9cb6252d4b93322b82a4ce8c87
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2017-06-08 21:33:29 -0400

    Create initial layout for index view (WIP)

--- racedbapp/view_index.py
+++ racedbapp/view_index.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
 #from django.db.models import Min, Q
 #from django import db
-#from collections import namedtuple
+from collections import namedtuple
 #import urllib
 #from . import utils
+from datetime import datetime
 from .models import *
 
 def index(request):
-    context = {}
+    today = datetime.today()
+    future_events = Event.objects.filter(date__gte=today).order_by('date', '-distance__km')
+    context = {'future_events': future_events,
+              }
     return render(request, 'racedbapp/index.html', context)
