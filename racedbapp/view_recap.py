f80c19eb2a80bb3bb86494885e8cc0e452984063
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2017-06-13 19:53:55 -0400

    Move individual to its own function

--- racedbapp/view_recap.py
+++ racedbapp/view_recap.py
@@ -15,7 +15,6 @@ def index(request, year, race_slug, distance_slug):
     namedevent = namedtuple('ne', ['event', 'individual_results', 'team_results'])
     namediresult = namedtuple('ni', ['place', 'female_athlete', 'female_time', 'male_athlete', 'male_time'])
     namedtresult = namedtuple('nt', ['team_category', 'top', 'winning_team', 'total_time', 'avg_time'])
-    individual_results = []
     if distance_slug == 'combined':
         event = False
         results = Endurraceresult.objects.filter(year=year).order_by('guntime')
@@ -28,25 +27,7 @@ def index(request, year, race_slug, distance_slug):
         results = Result.objects.filter(event_id=event.id)
         hasmasters = Result.objects.hasmasters(event)
         team_results = Teamresult.objects.of_event(event)
-    female_results = list(results.filter(gender="F").values_list('athlete', 'guntime')[0:3])
-    male_results = list(results.filter(gender="M").values_list('athlete', 'guntime')[0:3])
-    for i in range(1,4):
-        if i == 1:
-            rank = '1st OA'
-        elif i == 2:
-            rank = '2nd OA'
-        elif i == 3:
-            rank = '3rd OA'
-        female_guntime = female_results[i-1][1]
-        female_time = female_guntime - timedelta(microseconds=female_guntime.microseconds)
-        male_guntime = male_results[i-1][1]
-        male_time = male_guntime - timedelta(microseconds=male_guntime.microseconds)
-        individual_results.append(namediresult(rank, female_results[i-1][0], female_time, male_results[i-1][0], male_time))
-    if hasmasters:
-        if distance_slug == 'combined':
-            individual_results.append(Endurraceresult.objects.topmasters(year))
-        else:
-            individual_results.append(Result.objects.topmasters(event))
+    individual_results = get_individual_results(event, results, hasmasters, distance_slug, year=year)
     hill_results = False
     if race_slug == 'baden-road-races' and distance_slug == '7-mi':
         hill_results = []
@@ -85,3 +66,26 @@ def index(request, year, race_slug, distance_slug):
             return HttpResponse('Unknown format in URL', "text/html")
     else:
         return render(request, 'racedbapp/recap.html', context)
+
+def get_individual_results(event, results, hasmasters, distance_slug, year=False):
+    individual_results = []
+    female_results = list(results.filter(gender="F").values_list('athlete', 'guntime')[0:3])
+    male_results = list(results.filter(gender="M").values_list('athlete', 'guntime')[0:3])
+    for i in range(1,4):
+        if i == 1:
+            rank = '1st OA'
+        elif i == 2:
+            rank = '2nd OA'
+        elif i == 3:
+            rank = '3rd OA'
+        female_guntime = female_results[i-1][1]
+        female_time = female_guntime - timedelta(microseconds=female_guntime.microseconds)
+        male_guntime = male_results[i-1][1]
+        male_time = male_guntime - timedelta(microseconds=male_guntime.microseconds)
+        individual_results.append(namediresult(rank, female_results[i-1][0], female_time, male_results[i-1][0], male_time))
+    if hasmasters:
+        if distance_slug == 'combined':
+            individual_results.append(Endurraceresult.objects.topmasters(year))
+        else:
+            individual_results.append(Result.objects.topmasters(event))
+    return individual_results
