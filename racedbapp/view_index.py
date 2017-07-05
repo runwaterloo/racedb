9e93c10b390a581ae01aed9437159255d8f54946
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2017-07-05 17:15:07 -0400

    Pass records to index view
    Minimal formatting done to the template, this still needs a lot of work.

--- racedbapp/view_index.py
+++ racedbapp/view_index.py
@@ -5,16 +5,27 @@ from django.http import HttpResponse
 from collections import namedtuple
 import urllib
 import simplejson
-from . import view_recap, view_member
+from . import view_recap, view_member, view_shared
 from datetime import datetime
 from .models import *
 
+named_future_event = namedtuple('nfe', ['event',
+                                        'race',
+                                        'distance',
+                                        'records',
+                                        'team_records',
+                                        'hill_records',
+                                       ])
+
+named_event = namedtuple('ne', ['date', 'city'])
+named_race = namedtuple('nr', ['name', 'shortname', 'slug'])
+named_distance = namedtuple('nd', ['name', 'slug', 'km'])
+
 def index(request):
     qstring = urllib.parse.parse_qs(request.META['QUERY_STRING'])
-    today = datetime.today()
     lastraceday = get_lastraceday()
     memberinfo = get_memberinfo()
-    future_events = Event.objects.filter(date__gte=today).order_by('date', '-distance__km')
+    future_events = get_future_events()
     context = {'lastraceday': lastraceday,
                'memberinfo': memberinfo,
                'future_events': future_events,
@@ -52,6 +63,21 @@ def get_lastraceday():
         individual_recap = view_recap.get_individual_results(lde, event_results, hasmasters, distance_slug)
         lastraceday.append(named_lastraceday(lde, individual_recap, finishers))
     return lastraceday
+
+def get_future_events():
+    today = datetime.today()
+    future_events = []
+    dbfuture_events = Event.objects.filter(date__gte=today).order_by('date', '-distance__km')
+    for i in dbfuture_events:
+        event = named_event(i.date, i.city)
+        race = named_race(i.race.name, i.race.shortname, i.race.slug)
+        distance = named_distance(i.distance.name, i.distance.slug, i.distance.km)
+        numresults = Result.objects.filter(event__race=i.race, event__distance=i.distance).count()
+        records = False
+        if numresults > 0:
+            records, team_records, hill_records = view_shared.getracerecords(i.race, i.distance)
+        future_events.append(named_future_event(event, race, distance, records, team_records, hill_records))
+    return future_events
 
 def get_memberinfo():
     num_recent_badges = 4
