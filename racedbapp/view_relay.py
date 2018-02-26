d6b9faf7553d2134f62fe74d64e473cb6d30cbcf
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2018-02-25 20:41:16 -0500

    Return 404 if no relay results found for event

--- racedbapp/view_relay.py
+++ racedbapp/view_relay.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from urllib import parse
 from . import view_shared
 from .models import (
-    Event
+    Event,
+    Relay,
     )
 
 #from collections import namedtuple
@@ -15,9 +16,14 @@ from .models import (
 def index(request, year, race_slug, distance_slug):
     qstring = parse.parse_qs(request.META['QUERY_STRING'])
     event = get_event(year, race_slug, distance_slug)
+    relay_results = get_relay_results(event)
     team_categories = view_shared.get_team_categories(event)
     page = 'Relay'
-    pages = view_shared.get_pages(event, 'Relay', team_categories, laurier_relay_dict=True)
+    pages = view_shared.get_pages(
+        event,
+        'Relay',
+        team_categories,
+        laurier_relay_dict=True)
     context = {
         'event': EventV(event),
         'pages': pages,
@@ -36,6 +42,13 @@ def get_event(year, race_slug, distance_slug):
     except:
         raise Http404('Event not found')
     return event
+
+
+def get_relay_results(event):
+    relay_results = Relay.objects.filter(event=event)
+    if len(relay_results) == 0:
+        raise Http404('No results found')
+    return relay_results
 
 
 class EventV:
