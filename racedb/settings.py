5fcf3a4903e03bcb4f8d193c4b1bc9a4195f6065
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2018-04-10 21:05:33 -0400

    Remove clickjacking middleware for iframes

--- racedb/settings.py
+++ racedb/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'racedb.urls'
