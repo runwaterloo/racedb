a9e63ba647df5dd067be8e21093eb17ef47265ce
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2018-04-15 16:47:49 -0400

    Add time traveller (disabled)

--- racedbapp/view_boost.py
+++ racedbapp/view_boost.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Count
 from django.http import Http404
-from collections import namedtuple, OrderedDict
+from collections import namedtuple, defaultdict, OrderedDict
 from datetime import datetime
 from operator import attrgetter
 from urllib import parse
@@ -17,6 +17,7 @@ from .models import (
 named_filter = namedtuple('nf', ['current', 'choices'])
 named_choice = namedtuple('nc', ['name', 'url'])
 
+
 def index(request, year):
     config_values = (
         'boost_classic_points',
@@ -29,22 +30,25 @@ def index(request, year):
         'boost_pb_points',
         'nophoto_url',
         )
-    config_dict = dict(Config.objects.values_list('name', 'value').filter(name__in=config_values))
+    config_dict = dict(
+        Config.objects.values_list('name', 'value')
+        .filter(name__in=config_values)
+        )
     max_events = int(config_dict['boost_max_events'])
     max_endurrun = int(config_dict['boost_max_endurrun'])
     leaderboard_size = int(config_dict['boost_leaderboard_size'])
     nophoto_url = config_dict['nophoto_url']
     year = int(year)
-    qstring = parse.parse_qs(request.META['QUERY_STRING'])
-    qs_filter = get_qs_filter(qstring)
-    qs_date = get_qs_date(qstring)
     first_day = datetime(year, 1, 1).date()
     last_day = datetime(year, 12, 31).date()
+    qstring = parse.parse_qs(request.META['QUERY_STRING'])
+    qs_filter = get_qs_filter(qstring)
+    qs_date = ''
     if 'date' in qstring:
         try:
             ymd = [int(x) for x in qstring['date'][0].split('-')]
             qs_date = datetime(*ymd).date()
-        except:
+        except Exception:
             raise Http404('Invalid date')
         else:
             if first_day <= qs_date <= last_day:
@@ -72,7 +76,8 @@ def index(request, year):
                   gender_place__isnull=False,
                   rwmember__in=included_members).order_by('event__date')
     for i in dbresults:
-        battlers[i.rwmember_id].results.append(BResult(i, gender_finishers, config_dict, previous_races))
+        battlers[i.rwmember_id].results.append(
+            BResult(i, gender_finishers, config_dict, previous_races))
     for v in battlers.values():
         v.calculate(max_events, max_endurrun)
     gender_place_dict = {'F': 0, 'M': 0}
@@ -112,11 +117,11 @@ def index(request, year):
     leaderboard['Male Under 40'] = leaders['M40-']
     leaderboard['Female Over 40'] = leaders['F40+']
     leaderboard['Male Over 40'] = leaders['M40+']
-    standings_filter = get_standings_filter(qs_filter, year)
-    date_filter = get_date_filter(qs_date, first_day, last_day)
+    standings_filter = get_standings_filter(qs_filter, qs_date, year)
+    date_filter = get_date_filter(qs_date, year)
     if qs_member:
         context = {
-                   'config_dict' : config_dict,
+                   'config_dict': config_dict,
                    'qs_member': qs_member,
                    'member_results': member_results,
                    'year': year
@@ -127,6 +132,8 @@ def index(request, year):
                    'leaderboard': leaderboard,
                    'standings': standings,
                    'standings_filter': standings_filter,
+                   'date_filter': date_filter,
+                   'qs_date': qs_date,
                    'year': year
                   }
         return render(request, 'racedbapp/boost.html', context)
@@ -144,21 +151,32 @@ def get_qs_filter(qstring):
     return qs_filter
 
 
-def get_standings_filter(qs_filter, year):
+def get_standings_filter(qs_filter, qs_date, year):
     choices = []
-    choices.append(named_choice('', '/boost/{}'.format(year)))
+    append_date = ''
+    if qs_date != '':
+        append_date = '?date={}'.format(qs_date)
+    choices.append(named_choice('', '/boost/{}/{}'.format(year, append_date)))
+    if qs_date != '':
+        append_date = '&date={}'.format(qs_date)
     choices.append(
-        named_choice('Female', '/boost/{}/?filter=F'.format(year)))
+        named_choice(
+            'Female', '/boost/{}/?filter=F{}'.format(year, append_date)))
     choices.append(
-        named_choice('Male', '/boost/{}/?filter=M'.format(year)))
+        named_choice(
+            'Male', '/boost/{}/?filter=M{}'.format(year, append_date)))
     choices.append(
-        named_choice('F40-', '/boost/{}/?filter=F40-'.format(year)))
+        named_choice(
+            'F40-', '/boost/{}/?filter=F40-{}'.format(year, append_date)))
     choices.append(
-        named_choice('M40-', '/boost/{}/?filter=M40-'.format(year)))
+        named_choice(
+            'M40-', '/boost/{}/?filter=M40-{}'.format(year, append_date)))
     choices.append(
-        named_choice('F40+', '/boost/{}/?filter=F40%2B'.format(year)))
+        named_choice(
+            'F40+', '/boost/{}/?filter=F40%2B{}'.format(year, append_date)))
     choices.append(
-        named_choice('M40+', '/boost/{}/?filter=M40%2B'.format(year)))
+        named_choice(
+            'M40+', '/boost/{}/?filter=M40%2B{}'.format(year, append_date)))
     choices = [x for x in choices if x[0] != qs_filter]
     if qs_filter == 'F':
         current_choice = 'Female'
@@ -172,36 +190,58 @@ def get_standings_filter(qs_filter, year):
     return standings_filter
 
 
-def get_qs_date(qstring):
-    qs_date = ''
-    if 'date' in qstring:
-        raw_date = qstring['date'][0]
-        try:
-            date_filter = datetime.strptime(raw_date, '%Y-%m-%d')
-        except Exception:
-            raise Http404('Invalid date')
-    return qs_date
-
-
-def get_date_filter(qs_date, first_day, last_day):
-    year = first_day.year
-    end_date = last_day
-    if qs_date != '':
-        if qs_date < last_day:
-            end_date = qs_date
-    events = Event.objects.select_related().filter(date__range=(first_day, end_date)).annotate(num_events=Count('result')).filter(num_events__gte=1).order_by('-date')
+def get_date_filter(qs_date, year):
+    current_choice = False
+    start_date = datetime(year, 1, 1)
+    end_date = datetime(year, 12, 31)
+    last_event_date = Event.objects.filter(
+        date__range=(start_date, end_date)).order_by('-date')[0].date
+    if (datetime.now().date() - last_event_date).days > 7:
+        final = True
+    else:
+        final = False
+    events = (Event.objects
+              .select_related()
+              .filter(date__range=(start_date, end_date))
+              .annotate(num_events=Count('result'))
+              .filter(num_events__gte=1)
+              .order_by('-date')
+              )
+    if len(events) == 0:
+        raise Http404('No events found')
     choices = []
     dates_seen = []
+    racedates = defaultdict(list)
+    for e in events:
+        if e.date not in racedates[e.race]:
+            racedates[e.race].append(e.date)
     for e in events:
         if e.date in dates_seen:
             continue
-        if e.race.slug in ('endurrace', 'endurrun'):
-            choices.append(named_choice('{} {}'.format(e.race.shortname, e.distance.name), '/boost/{}/?date={}'.format(year, e.date)))
+        if len(racedates[e.race]) > 1:
+            choice = named_choice(
+                '{} {}'.format(e.race.shortname, e.distance.name),
+                '/boost/{}/?date={}'.format(year, e.date)
+                )
         else:
-            choices.append(named_choice(e.race.name, '/boost/{}/?date={}'.format(year, e.date)))
+            choice = named_choice(
+                e.race.name, '/boost/{}/?date={}'.format(year, e.date))
+        if e.date != qs_date:
+            choices.append(choice)
+        else:
+            current_choice = choice[0]
         dates_seen.append(e.date)
-    #choices = [x for x in choices if x[0] != qs_filter]
-    current_choice = choices[0][0]
+    first_choice = choices[0][0]
+    if final:
+        first_choice += ' (Final)'
+    else:
+        first_choice += ' (Current)'
+    del choices[0]
+    if current_choice:
+        choices.insert(
+            0, named_choice(first_choice, '/boost/{}/'.format(year)))
+    if not current_choice:
+        current_choice = first_choice
     date_filter = named_filter(current_choice, choices)
     return date_filter
 
@@ -218,7 +258,7 @@ def get_previous_races(year, included_members):
         if i.rwmember_id in previous_races:
             previous_races[i.rwmember_id].append(i.event.race)
         else:
-            previous_races[i.rwmember_id] = [i.event.race,]
+            previous_races[i.rwmember_id] = [i.event.race, ]
     return previous_races
 
 
@@ -305,11 +345,11 @@ class BResult:
         self.event_distance_slug = result.event.distance.slug
         self.guntime = result.guntime
         self.gender_place = result.gender_place
-        self.gender_finishers = gender_finishers[result.gender][result.event.id]
+        self.gender_finishers = gender_finishers[
+            result.gender][result.event.id]
         self.mp = ((1 - (self.gender_place
                    / self.gender_finishers))
                    * int(config_dict['boost_merit_max']))
-        total_boost = 0
         self.ditto_boost = self.pb_boost = self.classic_boost = 0
         if result.rwmember_id in previous_races:
             if result.event.race in previous_races[result.rwmember_id]:
