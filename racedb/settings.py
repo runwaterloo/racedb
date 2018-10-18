21ba7e77f6fa5e5b0dff7778bdd37e26da31cd4b
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2018-10-18 07:13:00 -0400

    Set DB host to  127.0.0.1

--- racedb/settings.py
+++ racedb/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'racedb',
         'USER': 'racedb',
         'PASSWORD': secrets.DB_PASSWORD,
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             },
