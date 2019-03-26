f9556e79ed6fba1eca3a34b524654765718077ca
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-03-25 22:29:17 -0400

    Blacken celery_beats.py

--- racedb/celery_beats.py
+++ racedb/celery_beats.py
@@ -1,12 +1,13 @@
 from celery.schedules import crontab
+
 CELERY_BEAT_SCHEDULE = {
-    'heartbeat': {
-        'task': 'racedbapp.tasks.heartbeat',
-        'schedule': crontab(minute="*/15")
+    "heartbeat": {
+        "task": "racedbapp.tasks.heartbeat",
+        "schedule": crontab(minute="*/15"),
     },
-    'photoupdate': {
-        'task': 'racedbapp.tasks.photoupdate',
-        'schedule': crontab(minute="54"),
-        'kwargs': {'request_date': "auto"}
+    "photoupdate": {
+        "task": "racedbapp.tasks.photoupdate",
+        "schedule": crontab(minute="54"),
+        "kwargs": {"request_date": "auto"},
     },
 }
