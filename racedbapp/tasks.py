faab92f0df4a9df929b082c3c2ce21c7c38c9e27
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-03-25 22:19:33 -0400

    Create task to update featured member in db

--- racedbapp/tasks.py
+++ racedbapp/tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
 import requests
 import os
-from . import secrets
-from . import process_photoupdate
-import logging                                                                           
-                                                                                         
-logger = logging.getLogger(__name__)  
+from . import process_photoupdate, secrets, view_member
+from .models import Config, Rwmember
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -16,11 +16,11 @@ def heartbeat():
 @shared_task
 def webhook():
     logger.info("Starting git pull...")
-    os.system("git pull https://gitlab.com/sl70176/racedb.git")    
+    os.system("git pull https://gitlab.com/sl70176/racedb.git")
     logger.info("Starting pip install...")
     os.system("pip install -r requirements.txt")
     logger.info("Touching wsgi.py")
-    os.system("touch racedb/wsgi.py")    
+    os.system("touch racedb/wsgi.py")
 
 
 @shared_task
@@ -28,10 +28,36 @@ def photoupdate(request_date=None, force=False):
     prod_ipaddr = secrets.prod_ipaddr
     my_ip = None
     try:
-        my_ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4", timeout=5).text.strip()
+        my_ip = requests.get(
+            "http://169.254.169.254/latest/meta-data/public-ipv4", timeout=5
+        ).text.strip()
     except Exception:
         pass
     if my_ip == prod_ipaddr or force:
         process_photoupdate.index(request_date)
     else:
         logger.info("Not production host, skipping photoupdate")
+
+
+@shared_task
+def update_featured_member_id():
+    """ Update featured_member_id to someone new """
+    db_member = Config.objects.filter(name="featured_member_id")
+    if db_member.count() == 0:
+        Config(name="featured_member_id", value=0).save()
+    current_featured_member = Config.objects.filter(name="featured_member_id")[:1][0]
+    current_featured_member_id = 0
+    if current_featured_member.value.isdigit():
+        current_featured_member_id = int(current_featured_member.value)
+    members = (
+        Rwmember.objects.filter(active=True)
+        .exclude(photourl=None)
+        .exclude(photourl="")
+        .order_by("?")
+    )
+    for member in members:
+        member_results, km = view_member.get_memberresults(member)
+        if km > 0 and member.id != current_featured_member_id:
+            break
+    current_featured_member.value = member.id
+    current_featured_member.save()
