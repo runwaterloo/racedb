e5553836ec1f1276eae4f290e4a4ca569a3c189d
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-03-28 21:23:57 -0400

    Update to handle docker, remove dev_to_prod

--- db_prod_to_dev.sh
+++ db_prod_to_dev.sh
@@ -4,5 +4,8 @@ read -p "Are you sure (y/n)? " -r
 echo    # (optional) move to a new line
 if [[ $REPLY = "y" ]]
 then
-    ssh roanne.scrw.ca "sudo /usr/bin/mysqldump racedb" | sudo mysql racedb
+    export PASSWORD=`cat /srv/racedb_secrets/MYSQL_ROOT_PASSWORD_FILE`
+    ssh roanne.scrw.ca "docker exec \`docker ps | grep racedb_db | awk '{print \$1}'\` /usr/bin/mysqldump -u root --password=\`cat /srv/racedb_secrets/MYSQL_ROOT_PASSWORD_FILE\` racedb" > /srv/racedb_secrets/restore.sql
+    docker exec `docker ps | grep racedb_db | awk '{print $1}'` /bin/bash -c "cat /run/secrets/restore.sql | /usr/bin/mysql -u root --password=$PASSWORD racedb"
+    rm -f /srv/racedb_secrets/restore.sql
 fi
