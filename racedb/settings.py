37bab25cbd4359cdc1ec1b6db4b5a226f7cced33
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-07-20 08:50:18 -0400

    Disable debug toolbar

--- racedb/settings.py
+++ racedb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 def show_toolbar(request):
@@ -33,8 +33,7 @@ def show_toolbar(request):
     else:
         return False
 
-
-DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": show_toolbar}
+#DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": show_toolbar}
 
 
 ALLOWED_HOSTS = ["*"]
