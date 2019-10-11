0e151d53300506f9f317aa0306344c2fca617fbb
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-10-11 17:18:20 +0000

    Improve deployment process
    This is just backend stuff, no visible changes.
    Also addresses 9Eqlz836.

--- deploy/swarm-dev/deploy.sh
+++ deploy/swarm-dev/deploy.sh
@@ -3,7 +3,9 @@ docker build -t racedb .
 IMAGE_ID=`docker image ls | grep racedb | grep latest | awk '{print $3}'`
 docker tag racedb:latest racedb:$IMAGE_ID
 echo $IMAGE_ID
-cp -f deploy/swarm-dev/racedb-stack-dev.yml /srv
-sed -i s/racedb:latest/racedb:$IMAGE_ID/g /srv/racedb-stack-dev.yml
-env BUILD=$IMAGE_ID docker stack deploy -c /srv/racedb-stack-dev.yml racedb
+cp -f deploy/swarm-dev/docker-compose-dev.yml /srv
+sed -i s/racedb:latest/racedb:$IMAGE_ID/g /srv/docker-compose-dev.yml
+env BUILD=$IMAGE_ID docker stack deploy -c /srv/docker-compose-dev.yml racedb
 docker system prune -f
+sleep 8
+docker exec -it `docker ps | grep racedb_gunicorn | awk '{print $1}'` ./manage.py clear_cache
---
+++ racedbapp/management/commands/clear_cache.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from django.core.management.base import BaseCommand, CommandError
+from racedbapp.tasks import clear_cache
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        clear_cache()
