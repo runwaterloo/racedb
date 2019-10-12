1632386ee09823013451cbee85cc0f1f8d07c4c1
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-10-12 01:31:09 +0000

    Grep for container that is still starting

--- deploy/swarm-prod/clear_cache.sh
+++ deploy/swarm-prod/clear_cache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 10
 docker ps | grep racedb_gunicorn
-docker exec -i `docker ps | grep racedb_gunicorn | awk '{print $1}'` /usr/local/bin/python /srv/racedb/manage.py clear_cache
+docker exec -i `docker ps | grep racedb_gunicorn | grep starting | awk '{print $1}'` /usr/local/bin/python /srv/racedb/manage.py clear_cache
 exit 0
