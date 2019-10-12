ca198132c6d1f44652d3eb7d308fdeadd7fd85f9
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-10-12 00:36:25 +0000

    Update clear_cache.sh

--- deploy/swarm-prod/clear_cache.sh
+++ deploy/swarm-prod/clear_cache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker exec --rm -i `docker ps | grep racedb_gunicorn | awk '{print $1}'` ./manage.py clear_cache
+sleep 10
+docker exec `docker ps | grep racedb_gunicorn | awk '{print $1}'` /usr/local/bin/python /srv/racedb/manage.py clear_cache
 exit 0
