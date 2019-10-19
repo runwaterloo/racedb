849b85383992d92ff2636cda50281429db1609b5
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-10-19 13:56:53 +0000

    Speed up dev deploy script
    [skip-ci]

--- deploy/swarm-dev/deploy.sh
+++ deploy/swarm-dev/deploy.sh
@@ -2,10 +2,7 @@
 docker build -t racedb .
 IMAGE_ID=`docker image ls | grep racedb | grep latest | awk '{print $3}'`
 docker tag racedb:latest racedb:$IMAGE_ID
-echo $IMAGE_ID
 cp -f deploy/swarm-dev/docker-compose-dev.yml /srv
 sed -i s/racedb:latest/racedb:$IMAGE_ID/g /srv/docker-compose-dev.yml
 env BUILD=$IMAGE_ID docker stack deploy -c /srv/docker-compose-dev.yml racedb
-docker system prune -f
-sleep 10
-docker exec -it `docker ps | grep racedb_gunicorn | awk '{print $1}'` ./manage.py clear_cache
+docker service update --force racedb_redis -d
