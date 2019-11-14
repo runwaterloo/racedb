5b27bb4a5524c75c35ecd464a3064be34fd25cea
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2019-11-14 22:29:38 +0000

    Update clear_cache.sh to include prod settings

--- deploy/swarm-prod/clear_cache.sh
+++ deploy/swarm-prod/clear_cache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -i `docker ps | grep racedb_gunicorn | head -n 1 | awk '{print $1}'` /usr/local/bin/python /srv/racedb/manage.py clear_cache
+docker exec -i `docker ps | grep racedb_gunicorn | head -n 1 | awk '{print $1}'` /usr/local/bin/python /srv/racedb/manage.py clear_cache --settings=racedb.settings.prod
