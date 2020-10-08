192a6fba9362f6c3bbfa514208daaf3059d7784e
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2020-10-08 15:15:44 +0000

    Advance K3S installation a bit further

--- deploy/k3s/k3s_install.sh
+++ deploy/k3s/k3s_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-INSTALL_K3S_VERSION=`cat ./K3S_VERSION`
-INSTALL_K3S_EXEC="--tls-san results.runwaterloo.com --tls-san $PUBLIC_IP"
+export PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+export INSTALL_K3S_VERSION=`cat ./K3S_VERSION`
+export INSTALL_K3S_EXEC="--tls-san results.runwaterloo.com --tls-san $PUBLIC_IP"
 curl -sfL https://get.k3s.io | sh -
+aws s3 cp /etc/rancher/k3s/k3s.yaml s3://racedb-private/k3s/k3s.yaml
