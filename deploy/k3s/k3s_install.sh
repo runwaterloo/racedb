7772bc00df6407c9dff5a8011db3add0f6cd81fe
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2020-10-09 18:12:52 +0000

    Do not deploy Traefik

--- deploy/k3s/k3s_install.sh
+++ deploy/k3s/k3s_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 export INSTALL_K3S_VERSION=`cat ./K3S_VERSION`
-export INSTALL_K3S_EXEC="--tls-san results.runwaterloo.com --tls-san $PUBLIC_IP"
+export INSTALL_K3S_EXEC="--no-deploy=traefik --tls-san results.runwaterloo.com --tls-san $PUBLIC_IP"
 curl -sfL https://get.k3s.io | sh -
 aws s3 cp /etc/rancher/k3s/k3s.yaml s3://racedb-private/k3s/k3s.yaml
