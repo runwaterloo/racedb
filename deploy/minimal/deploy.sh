505bd7a9f2783b32c032d70c424c2cd92fb53433
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2020-11-08 20:25:09 +0000

    Bump mariadb to 10.3.23 in pipeline

--- deploy/minimal/deploy.sh
+++ deploy/minimal/deploy.sh
@@ -12,6 +12,8 @@ echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositorie
 apk add docker-compose mysql-client py-pip
 /usr/bin/pip3 install awscli
 aws s3 cp ${RACEDB_DB_BACKUP} .
+echo "GRANT ALL PRIVILEGES ON *.* TO 'racedb'@'%';" | mysql -h mariadb -u root -pCHANGEME
+echo "FLUSH PRIVILEGES;" | mysql -h mariadb -u root -pCHANGEME
 zcat ./racedb.latest.sql.gz | mysql -h mariadb -u racedb -pCHANGEME racedb
 MARIADB_IP=`grep mariadb /etc/hosts | awk '{print $1}'`
 sed -i s/MARIADB_IP/$MARIADB_IP/g deploy/minimal/docker-compose-min.yml
