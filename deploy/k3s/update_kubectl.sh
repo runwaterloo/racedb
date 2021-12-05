c4f5e1940e04934748fe7653058dc3baa98ca23a
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2021-12-05 11:14:32 -0500

    Remove old K3S deployment bits

--- deploy/k3s/update_kubectl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BUCKET=$1
-IPADDR=$2
-mv ~/.kube/config ~/.kube/config.$$
-aws --profile js s3 cp s3://${BUCKET}/k3s/k3s.yaml ~/.kube/config
-sed -i "s/127.0.0.1/${IPADDR}/g" ~/.kube/config
