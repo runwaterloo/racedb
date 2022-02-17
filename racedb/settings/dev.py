6a9c994f932de2235077d754b6928662efb24494
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2022-02-17 16:03:19 -0500

    Change redis for dev instance

--- racedb/settings/dev.py
+++ racedb/settings/dev.py
@@ -9,14 +9,14 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://racedb-redis:6379/1",
+        "LOCATION": "redis://racedbdev-redis:6379/1",
         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         "TIMEOUT": None,
     }
 }
 
-CELERY_BROKER_URL = "redis://racedb-redis:6379"
-CELERY_RESULT_BACKEND = "redis://racedb-redis:6379"
+CELERY_BROKER_URL = "redis://racedbdev-redis:6379"
+CELERY_RESULT_BACKEND = "redis://racedbdev-redis:6379"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
