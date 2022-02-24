3a1300ff3f1f03cbb55a47e28be14dfcf79f8b82
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2022-02-24 03:27:10 +0000

    Fix Helm installation issues

--- deploy/helm/install_helm.sh
+++ deploy/helm/install_helm.sh
@@ -2,6 +2,7 @@
 
 export HELM_VERSION=`cat ./HELM_VERSION`
 curl -fsSL -o helm.tar.gz https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
+mkdir -p /usr/local/bin
 tar -zxvf helm.tar.gz -C /usr/local/bin/
 chmod +x /usr/local/bin/helm
 rm -f helm.tar.gz
