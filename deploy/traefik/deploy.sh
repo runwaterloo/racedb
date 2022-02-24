be538fbf0d89d6b1dd7af996d21624c61787f042
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2022-02-24 04:37:17 +0000

    Bump traefik to chart 10.14.2 and pin

--- deploy/traefik/deploy.sh
+++ deploy/traefik/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export TRAEFIK_CHART_VERSION=`cat ./TRAEFIK_CHART_VERSION`
 mkdir -p /pv/traefik 2>/dev/null
 chmod 777 /pv/traefik
 kubectl apply -f pv.yaml
 kubectl apply -f pvc.yaml
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
-helm upgrade traefik traefik/traefik --values values.yaml --install 
+helm upgrade traefik traefik/traefik --version $TRAEFIK_CHART_VERSION --values values.yaml --install
