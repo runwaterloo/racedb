f1d53fa9a23e56df60892905b9a85ddf8c551022
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2022-05-16 18:15:52 +0000

    Fix DB refresh script [skip ci]

--- deploy/misc/refresh_dev_db.sh
+++ deploy/misc/refresh_dev_db.sh
@@ -5,11 +5,13 @@ set -e
 set -u
 
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-DBHOST=`cat /srv/racedb_secrets_dev/secrets.py | grep DB_HOST | awk '{print $3}' | awk -F"'" '{print $2}'`
-DBPW=`cat /srv/racedb_secrets_dev/secrets.py | grep DB_PASSWORD | awk '{print $3}' | awk -F"'" '{print $2}'`
+DEVDBHOST=`cat /srv/racedb_secrets_dev/secrets.py | grep DB_HOST | awk '{print $3}' | awk -F"'" '{print $2}'`
+DEVDBPW=`cat /srv/racedb_secrets_dev/secrets.py | grep DB_PASSWORD | awk '{print $3}' | awk -F"'" '{print $2}'`
 DEVWEBPOD=`kubectl get pods | grep racedbdev-web | awk '{print $1}'`
-
-aws s3 cp --quiet s3://racedb-private/database_backup/racedb.latest.sql.gz /tmp
-gunzip /tmp/racedb.latest.sql.gz
-cat /tmp/racedb.latest.sql | kubectl exec $DEVWEBPOD -- mysql -h $DBHOST -u racedbdev -p$DBPW racedbdev
-rm -f /tmp/racedb.latest.sql
+PRODDBHOST=`cat /srv/racedb_secrets/secrets.py | grep DB_HOST | awk '{print $3}' | awk -F"'" '{print $2}'`
+PRODDBPW=`cat /srv/racedb_secrets/secrets.py | grep DB_PASSWORD | awk '{print $3}' | awk -F"'" '{print $2}'`
+PRODWEBPOD=`kubectl get pods | grep racedb-web | awk '{print $1}'`
+kubectl exec $PRODWEBPOD -c racedb -- mysqldump -h $PRODDBHOST -u racedb -p$PRODDBPW racedb > /tmp/dump.sql > /tmp/dump.sql
+cat /tmp/dump.sql | kubectl exec -i $DEVWEBPOD -- mysql -h $DEVDBHOST -u racedbdev -p$DEVDBPW racedbdev
+kubectl exec $DEVWEBPOD -- ./manage.py clear_cache
+rm -f /tmp/dump.sql
