39e57d2aeb2240f574541c7afcea3ea5e83c37c9
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2024-05-21 21:06:56 +0000

    Fix deploy.sh

--- deploy/minimal/deploy.sh
+++ deploy/minimal/deploy.sh
@@ -21,7 +21,7 @@ aws s3 cp ${RACEDB_DB_BACKUP} .
 echo "GRANT ALL PRIVILEGES ON *.* TO 'racedb'@'%';" | mysql -h mariadb -u root -pCHANGEME
 echo "FLUSH PRIVILEGES;" | mysql -h mariadb -u root -pCHANGEME
 zcat ./racedb.latest.sql.gz | mysql -h mariadb -u racedb -pCHANGEME racedb
-MARIADB_IP=`grep mariadb /etc/hosts | awk '{print $1}'`
+MARIADB_IP=`grep mariadb /etc/hosts | head -n 1 | awk '{print $1}'`
 sed -i s/MARIADB_IP/$MARIADB_IP/g deploy/minimal/docker-compose-min.yml
 cp racedb/secrets.py.sample racedb/secrets.py
 cd deploy/minimal
