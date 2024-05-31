4612254a5650230a0e156db54b9d23940eaf0f21
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2024-05-31 14:20:22 +0000

    Eliminate version.txt

--- deploy/gitlab/tag.sh
+++ deploy/gitlab/tag.sh
@@ -3,17 +3,8 @@
 # install git package
 apk add git -f
 
-# confirm that version.txt was modified
-if ! git diff-tree --no-commit-id --name-only -r ${CI_COMMIT_SHORT_SHA} | grep version.txt; then
-  echo "ERROR: version.txt must be updated to run pipeline on main"
-  exit 1
-fi
-
-export VERSION=`cat version.txt | xargs`
-if [ $(git tag -l "v${VERSION}") ]; then
-    echo "Tag v${VERSION} already exists, no need to repeat tasks."
-    exit 0
-fi
+# set version
+export VERSION=$(date +'%y.%-m.%-d%H%M%S')
 
 # pull image
 docker pull $CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA
--- update-dependencies.sh
+++ update-dependencies.sh
@@ -19,32 +19,6 @@ if git diff --exit-code --quiet -- requirements.txt; then
 else
     echo "requirements.txt updated, creating Merge Request"
 
-    # read the version from the file
-    version=$(cat version.txt)
-
-    # extract the parts of the version number
-    old_year=$(echo $version | cut -d'.' -f1)
-    old_month=$(echo $version | cut -d'.' -f2)
-    old_increment=$(echo $version | cut -d'.' -f3)
-
-    # get the current date
-    current_year=$(date +"%y")
-    current_month=$(date +"%-m")
-
-    # compare the dates and update the version accordingly
-    if [[ "$current_year" -gt "$old_year" ]] || [[ "$current_year" -eq "$old_year" && "$current_month" -gt "$old_month" ]]
-    then
-        # the current year or month is greater than the old one, so reset the increment
-        new_version="${current_year}.${current_month}.0"
-    else
-        # the current year and month match the old ones, so increment the old increment
-        new_increment=$((old_increment+1))
-        new_version="${old_year}.${old_month}.${new_increment}"
-    fi
-
-    # save the new version to the file
-    echo $new_version > version.txt
-
     # Git configuration (replace with your username and email)
     git config --global user.name "$DEPUP_NAME"
     git config --global user.email "$DEPUP_EMAIL"
@@ -52,7 +26,7 @@ else
     # Create a new branch and commit changes
     BRANCH="update-dependencies-`date +%s`"
     git checkout -b $BRANCH
-    git add requirements.txt version.txt
+    git add requirements.txt
     git commit -m "Update Python package dependencies"
 
     # Push the branch to the remote repository
