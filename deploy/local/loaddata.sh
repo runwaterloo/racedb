3bf8f85443e736676409937d020fccca277d1f02
Jordan Van Dyk <jordanvandyk7@gmail.com>
2024-06-01 14:59:27 +0000

    Fix load data script for windows

--- deploy/local/loaddata.sh
+++ deploy/local/loaddata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker exec racedb-web sh -c 'find racedbapp/fixtures -name "*.yaml" | xargs ./manage.py loaddata --settings=racedb.settings.min'
-docker exec racedb-web sh -c './manage.py generate_fake_rwmembers --settings=racedb.settings.min'
-docker exec racedb-web sh -c './manage.py generate_fake_results --settings=racedb.settings.min'
+docker exec racedb-web sh -c 'find racedbapp/fixtures -name "*.yaml" | xargs python ./manage.py loaddata --settings=racedb.settings.min'
+docker exec racedb-web sh -c 'python ./manage.py generate_fake_rwmembers --settings=racedb.settings.min'
+docker exec racedb-web sh -c 'python ./manage.py generate_fake_results --settings=racedb.settings.min'
