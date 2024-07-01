9e9377ce24ffa06c6009841bc6fc8871449d462f
Jordan Van Dyk <jordanvandyk7@gmail.com>
2024-07-01 15:05:28 +0000

    Sort members

--- racedbapp/view_members.py
+++ racedbapp/view_members.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-
-# from urllib import parse
-# from collections import namedtuple
+from django.http.request import HttpRequest
+from django.template.defaulttags import register
 from .models import *
+from datetime import date
 
-# from operator import attrgetter
 
-
-def index(request):
-    members = Rwmember.objects.filter(active=True)
+def index(request: HttpRequest):
+    members,memberEventDates = order_members(Rwmember.objects.filter(active=True),  request.GET.get("ordering", "id"))
     try:
         no_camera_tag = Rwmembertag.objects.get(name="no-profile-camera")
     except:
@@ -18,5 +16,28 @@ def index(request):
     context = {
         "members": members,
         "no_camera_members": no_camera_members,
+        "member_event_dates" : memberEventDates
     }
     return render(request, "racedbapp/members.html", context)
+
+def order_members(members, ordering):
+    results = Result.objects.values('rwmember').exclude(rwmember__isnull=True).annotate(oldest_date=Min('event__date'))
+    memberEventDates = {}
+    for result in results:
+        if result['rwmember'] in memberEventDates and memberEventDates[result['rwmember']] < result['oldest_date']:
+            continue
+        memberEventDates[result['rwmember']] = result['oldest_date']
+    if ordering == "date":
+        members = sorted(members,key=lambda member: memberEventDates.get(member.id,date.today()))
+    return members,memberEventDates
+
+@register.filter
+def get_date(dictionary, key):
+    date = dictionary.get(key, "")
+    if date != "":
+        date = date.strftime("%Y")
+    return date
+
+@register.filter
+def get_ranking(list, item):
+    return list.index(item) + 1
