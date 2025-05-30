525650a1dc64a828002b0b3efc1c2e8b773d8412
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2025-05-30 06:49:08 -0400

    Re-add django_prometheus path

--- racedb/urls.py
+++ racedb/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import include
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import re_path, path
 
 urlpatterns = [
+    path('', include('django_prometheus.urls')),
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^", include("racedbapp.urls")),
 ]
