b4b9770407665f912ec46a869e4d3190caeb9455
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2025-06-01 19:27:39 +0000

    Fix logic for pushing to racedb

--- deploy/helm/upgrade.sh
+++ deploy/helm/upgrade.sh
@@ -17,6 +17,17 @@ export SLACK_GITLAB_WEBHOOK=$(cat /root/SLACK_GITLAB_WEBHOOK)
 # move to directory
 cd /srv/racedb/deploy/helm
 
+# pre-flight check
+SHA_FILE="/tmp/latest_pipeline_sha.txt"
+read latest_pipeline_status latest_pipeline_sha < <(curl -s --header "PRIVATE-TOKEN: ${PERSONAL_ACCESS_TOKEN}" \
+  "https://gitlab.com/api/v4/projects/${PROJECT_ID}/pipelines?per_page=1" | jq -r '.[0] | "\(.status) \(.sha[0:8])"')
+if [ -f "$SHA_FILE" ] && grep -q "$latest_pipeline_sha" "$SHA_FILE"; then
+  echo "SHA hasn't changed since last run ($latest_pipeline_sha), exiting."
+  exit 0
+elif [ "$latest_pipeline_status" = "success" ]; then
+  echo "$latest_pipeline_sha" > "$SHA_FILE"
+fi
+
 # get current tag from deployment
 CURRENT_TAG=$(kubectl get deployment racedb-web -o jsonpath="{.spec.template.spec.containers[*].image}" | awk -F":" '{print $2}')
 echo "Current tag: $CURRENT_TAG"
@@ -80,5 +91,41 @@ if [ "$CURRENT_TAG" != "$LATEST_TAG" ]; then
   fi
 
 else
-  echo "Already running the latest tag: $CURRENT_TAG"
+  echo "RRW already running the latest tag: $CURRENT_TAG"
+
+  echo "Checking for a [push dev] commit for racedb"
+
+  CURRENT_TAG=$(kubectl get deployment racedbdev-web -o jsonpath="{.spec.template.spec.containers[*].image}" | awk -F":" '{print $2}')
+  echo "Current tag: $CURRENT_TAG"
+
+  if [ "$latest_pipeline_status" = "success" ]; then
+    if [ "$CURRENT_TAG" = "$latest_pipeline_sha" ]; then
+      echo "Current deployed tag ($CURRENT_TAG) is the same as latest pipeline SHA. Skipping deployment."
+      exit 0
+    fi
+
+    commit_message=$(curl -s --header "PRIVATE-TOKEN: ${PERSONAL_ACCESS_TOKEN}" \
+      "https://gitlab.com/api/v4/projects/${PROJECT_ID}/repository/commits/$latest_pipeline_sha" | jq -r '.message')
+
+    if echo "$commit_message" | grep -q '\[push dev\]'; then
+      echo "Latest commit contains [push dev], updating racedbdev"
+
+      echo "Deploying new tag: $latest_pipeline_sha to racedbdev"
+      if ! helm upgrade --install racedbdev . \
+        --values values-racedb.yaml \
+        --set image.tag="${latest_pipeline_sha}" \
+        --wait --timeout 5m; then
+
+        curl -sS -X POST -H 'Content-type: application/json' \
+          --data "{\"text\":\"❌ Failed to upgrade *racedbdev* to *${latest_pipeline_sha}*\"}" \
+          "$SLACK_GITLAB_WEBHOOK"
+        exit 1
+      fi
+    else
+      echo "Latest commit message does NOT contain [push dev]"
+    fi
+  else
+    echo "Latest pipeline did not succeed (status: $latest_pipeline_status), SHA: $latest_pipeline_sha"
+  fi
+
 fi
