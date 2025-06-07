822e73ee34a415dedaa8ded0a88d9d7a5bd42bd2
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2025-06-07 10:19:44 -0400

    Install drf-nested-routers

--- racedbapp/tests/api/test_drf_import.py
+++ racedbapp/tests/api/test_drf_import.py
@@ -3,3 +3,10 @@ def test_drf_can_be_imported():
         import rest_framework  # noqa: F401
     except ImportError:
         assert False, "Django REST framework is not installed or cannot be imported"
+
+
+def test_drf_nested_can_be_imported():
+    try:
+        import rest_framework_nested.routers  # noqa: F401
+    except ImportError:
+        assert False, "drf-nested-routers is not installed or cannot be imported"
