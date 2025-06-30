4cb99cbbfc2e1c3a9187b35a5d6553842eb26668
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2025-06-30 13:36:29 -0400

    chore(deps): upgrade infrastructure components (Jun 2025) (#46) (#49)
    K3S: v1.32.3+k3s1 -> v1.33.1+k3s1
    Helm: v3.17.3 -> v3.18.3
    Traefik: 35.0.0 -> 36.2.0
    Grafana Alloy: 1.0.3 -> 1.1.2

--- deploy/alloy/deploy.sh
+++ deploy/alloy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export ALLOY_VERSION=`cat ./ALLOY_VERSION | head -n 1`
+export ALLOY_CHART_VERSION=`cat ./ALLOY_CHART_VERSION | head -n 1`
 export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm upgrade alloy grafana/alloy --version $ALLOY_VERSION --values values.yaml --install --wait --timeout 5m
+helm upgrade alloy grafana/alloy --version $ALLOY_CHART_VERSION --values values.yaml --install --wait --timeout 5m
