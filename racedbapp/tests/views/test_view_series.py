4191580b43ed4a57872b38fe5a5d25f230a91954
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2025-07-20 11:30:35 -0400

    fix(series): allow filters to work on age or category #83

--- racedbapp/tests/views/test_view_series.py
+++ racedbapp/tests/views/test_view_series.py
@@ -1,6 +1,9 @@
 import pytest
 from rest_framework.test import APIClient
 
+from racedbapp.models import Result
+from racedbapp.view_series import set_ismaster_from_result
+
 
 @pytest.mark.django_db
 def test_series_endpoint_missing():
@@ -17,3 +20,41 @@ def test_series_endpoint_success(create_series):
     url = f"/series/{series.slug}/"
     response = client.get(url)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_series_endpoint_with_filter(create_series):
+    client = APIClient()
+    series = create_series()
+    first_result = Result.objects.first()
+    first_result.age = 40
+    first_result.save()
+    for filter in (
+        "Female",
+        "Male",
+        "Masters",
+        "F-Masters",
+        "M-Masters",
+        "some-category",
+    ):
+        url = f"/series/{series.slug}/?filter={filter}"
+        response = client.get(url)
+        assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_set_ismaster_from_result(create_result):
+    result = create_result()
+    scenarios = [
+        {"age": None, "category_ismasters": False, "expected_result": False},
+        {"age": None, "category_ismasters": True, "expected_result": True},
+        {"age": 39, "category_ismasters": False, "expected_result": False},
+        {"age": 39, "category_ismasters": True, "expected_result": False},
+        {"age": 40, "category_ismasters": False, "expected_result": True},
+        {"age": 40, "category_ismasters": True, "expected_result": True},
+    ]
+    for scenario in scenarios:
+        result.age = scenario["age"]
+        result.category.ismasters = scenario["category_ismasters"]
+        result.save()
+        assert set_ismaster_from_result(result) is scenario["expected_result"]
