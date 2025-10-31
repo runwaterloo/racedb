e5bd4af56abbb4c51e473f9455989fdb0908f683
Sam Lalonde <217337147+sam-lalonde@users.noreply.github.com>
2025-10-30 21:18:45 -0400

    chore(database): migrate database scripts and configurations to PostgreSQL. Fixes #174 (#176)

--- deploy/misc/refresh_dev_db.sh
+++ deploy/misc/refresh_dev_db.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
-# replace the dev database with the latest prod backup in S3
+# refresh the dev database with prod
+set -eu
 
-set -e
-set -u
+# Name of the database and role
+DEV_DB_NAME="racedbdev"
 
-KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-DEVDBHOST=`cat /srv/racedb_secrets_dev/secrets.py | grep DB_HOST | awk '{print $3}' | awk -F"'" '{print $2}'`
-DEVDBPW=`cat /srv/racedb_secrets_dev/secrets.py | grep DB_PASSWORD | awk '{print $3}' | awk -F"'" '{print $2}'`
-DEVWEBPOD=`kubectl get pods | grep racedbdev-web | awk '{print $1}'`
-PRODDBHOST=`cat /srv/racedb_secrets/secrets.py | grep DB_HOST | awk '{print $3}' | awk -F"'" '{print $2}'`
-PRODDBPW=`cat /srv/racedb_secrets/secrets.py | grep DB_PASSWORD | awk '{print $3}' | awk -F"'" '{print $2}'`
-PRODWEBPOD=`kubectl get pods | grep racedb-web | awk '{print $1}'`
-kubectl exec $PRODWEBPOD -c racedb -- mysqldump -h $PRODDBHOST -u racedb -p$PRODDBPW racedb > /tmp/dump.sql > /tmp/dump.sql
-cat /tmp/dump.sql | kubectl exec -i $DEVWEBPOD -- mysql -h $DEVDBHOST -u racedbdev -p$DEVDBPW racedbdev
-kubectl exec $DEVWEBPOD -- ./manage.py clear_cache
-rm -f /tmp/dump.sql
+# Password for dev role
+DEV_PASSWORD=$(grep DB_PASSWORD /srv/racedb_secrets_dev/secrets.py | awk -F"'" '{print $2}')
+
+# Check if the role exists
+ROLE_EXISTS=$(kubectl exec -i postgres-0 -- psql -U racedb -d postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$DEV_DB_NAME';")
+
+# If it doesn't exist, create it
+if [ "$ROLE_EXISTS" != "1" ]; then
+    echo "Role $DEV_DB_NAME does not exist. Creating..."
+        kubectl exec -i postgres-0 -- psql -U racedb -d postgres -c "CREATE ROLE $DEV_DB_NAME WITH LOGIN PASSWORD '$DEV_PASSWORD';"
+else
+    echo "Role $DEV_DB_NAME already exists. Skipping creation."
+fi
+
+# Check if the database exists
+DB_EXISTS=$(kubectl exec -i postgres-0 -- psql -U racedb -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$DEV_DB_NAME';")
+
+# If it doesn't exist, create it
+if [ "$DB_EXISTS" != "1" ]; then
+    echo "Database $DEV_DB_NAME does not exist. Creating..."
+    kubectl exec -i postgres-0 -- psql -U racedb -d postgres -c "CREATE DATABASE $DEV_DB_NAME OWNER $DEV_DB_NAME;"
+else
+    echo "Database $DEV_DB_NAME already exists. Skipping creation."
+fi
+
+echo "Importing data from prod to dev..."
+kubectl exec -i postgres-0 -- pg_dump -U racedb -d racedb --clean --if-exists --no-owner --no-acl --no-comments | kubectl exec -i postgres-0 -- psql -q -U $DEV_DB_NAME -d $DEV_DB_NAME >/dev/null
+echo "Import complete."
+
+# Clear cache in dev pod if it exists
+DEV_POD=$(kubectl get pod -l "app.kubernetes.io/instance=racedbdev,app.kubernetes.io/task=web" -o jsonpath="{.items[0].metadata.name}" 2>/dev/null || true)
+if [ -n "$DEV_POD" ]; then
+    echo "Clearing cache in dev pod..."
+    kubectl exec -it "$DEV_POD" -- ./manage.py clear_cache
+    echo "Cache cleared."
+fi
--- deploy/postgres/create_secret.sh
+++ deploy/postgres/create_secret.sh
@@ -9,12 +9,7 @@ fi
 
 echo "Creating 'postgres-secret'..."
 POSTGRES_PASSWORD=$(grep DB_PASSWORD /srv/racedb_secrets/secrets.py | awk -F"'" '{print $2}')
-POSTGRES_DEV_PASSWORD=$(grep DB_PASSWORD /srv/racedb_secrets_dev/secrets.py | awk -F"'" '{print $2}')
-
 kubectl create secret generic postgres-secret \
   --from-literal=username="racedb" \
   --from-literal=password="$POSTGRES_PASSWORD" \
-  --from-literal=dev_db="racedbdev" \
-  --from-literal=dev_username="racedbdev" \
-  --from-literal=dev_password="$POSTGRES_DEV_PASSWORD" \
   --dry-run=client -o yaml | kubectl apply -f -
